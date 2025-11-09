70eaf17ce7288b63311821cff06bd9c5bebcd4f7
Muhammad Rivaldo <rivaldomuhammad219@gmail.com>
2025-11-09 12:40:33 +0700

    final fix: keep express alive on railway

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
   res.send("API Running...");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 console.log("Starting Express server...");
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
