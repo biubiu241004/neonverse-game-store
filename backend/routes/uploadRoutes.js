aee1142d1660d5b15c02e3a1ae0fa470231bb7a3
Muhammad Rivaldo <rivaldomuhammad219@gmail.com>
2025-11-13 19:47:18 +0700

    commit img biar bisa di liat online

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import multer from "multer";
 import path from "path";
+import cloudinary from "../config/cloudinary.js";
 
 const router = express.Router();
 
@@ -30,10 +31,16 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({ storage, fileFilter });
 
 // 📸 Route upload
-router.post("/", upload.single("image"), (req, res) => {
-  res.json({
-    imageUrl: `/uploads/${req.file.filename}`,
-  });
+router.post("/", upload.single("image"), async (req, res) => {
+  try {
+    const result = await cloudinary.uploader.upload(req.file.path, {
+      folder: "valdo_store"
+    });
+
+    res.json({ imageUrl: result.secure_url });
+  } catch (err) {
+    res.status(500).json({ message: "Upload gagal", error: err.message });
+  }
 });
 
 export default router;
